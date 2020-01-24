8f415f45c1f33eacc213eef33280b1f3ff375aa2
DelrioRamirez4321 <delrioramirez7813@gmail.com>
2020-01-24 09:57:34 -0800

    did question 10

--- script.js
+++ script.js
@@ -37,5 +37,6 @@ console.log(10  *10 > 50 && 5 * 5 < 50);
 console.log(1+ 2 > 5 || 1 * 10 < 5);
 
 // 10. Write code that demonstrates an automatic type conversion as shown in Chapter 01.
+console.log(typeof "code");
 
 
